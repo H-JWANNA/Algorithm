ea50946813d2a0c73c02d583ab87ef2b77dd4f79
H-JWANNA <hongjjwan@gmail.com>
2022-09-17 23:34:00 +0900

    A+B - 2

--- src/baekjoon/bronze/Main_2558.java
+++ src/baekjoon/bronze/Main_2558.java
@@ -1,8 +1,6 @@
 package baekjoon.bronze;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.io.*;
 
 public class Main_2558 {
     public static void main(String[] args) throws IOException {
