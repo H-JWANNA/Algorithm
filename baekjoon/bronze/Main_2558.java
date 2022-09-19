8e6693d8efcfdb1c59fc986fb5a1bffb8f2de76e
H-JWANNA <hongjjwan@gmail.com>
2022-09-19 15:26:09 +0900

    A+B - 2

---
+++ baekjoon/bronze/Main_2558.java
@@ -0,0 +1,17 @@
+package baekjoon.bronze;
+
+import java.io.*;
+
+public class Main_2558 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        int result = 0;
+
+        for (int i = 0 ; i < 2; i++) {
+            int num = Integer.parseInt(input.readLine());
+            result += num;
+        }
+
+        System.out.println(result);
+    }
+}
