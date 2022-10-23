2df7757df151845521e4788d2eb923641c16967c
H-JWANNA <hongjjwan@gmail.com>
2022-10-23 13:17:57 +0900

    서울에서 김서방 찾기

---
+++ Programmers/Lv1/Solution_12919.java
@@ -0,0 +1,20 @@
+package Lv1;
+
+public class Solution_12919 {
+    public String solution(String[] seoul) {
+        StringBuilder answer = new StringBuilder();
+
+        answer.append("김서방은 ");
+
+        for(int i = 0; i < seoul.length; i++) {
+            if(seoul[i].equals("Kim")) {
+                answer.append(i);
+                break;
+            }
+        }
+
+        answer.append("에 있다.");
+
+        return answer.toString();
+    }
+}
