ff7be2673d704366497b41aff6d1bbfb90b34c37
H-JWANNA <hongjjwan@gmail.com>
2022-10-31 22:48:07 +0900

    핸드폰 번호 가리기

---
+++ Programmers/Lv1/Solution_12948.java
@@ -0,0 +1,18 @@
+package Lv1;
+
+public class Solution_12948 {
+    public String solution(String phone_number) {
+        StringBuilder sb = new StringBuilder();
+        int len = phone_number.length();
+
+        for (int i = 0; i < len - 4; i++) {
+            sb.append("*");
+        }
+
+        for (int i = len - 4; i < len; i++) {
+            sb.append(phone_number.charAt(i));
+        }
+
+        return sb.toString();
+    }
+}
