94cacb86e8841a45a15e7569d7bdd76f3362ee46
H-JWANNA <hongjjwan@gmail.com>
2022-12-24 22:11:23 +0900

    내적

---
+++ Programmers/Lv1/Solution_70128.java
@@ -0,0 +1,14 @@
+package Lv1;
+
+public class Solution_70128 {
+	public int solution(int[] a, int[] b) {
+
+		int answer = 0;
+
+		for (int i = 0; i < a.length; i++) {
+			answer += (a[i] * b[i]);
+		}
+
+		return answer;
+	}
+}
