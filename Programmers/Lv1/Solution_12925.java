2d60aa97a36a0af392a0c0a19f0327ea4cbf81f6
H-JWANNA <hongjjwan@gmail.com>
2023-02-06 22:17:46 +0900

    문자열을 정수로 바꾸기

---
+++ Programmers/Lv1/Solution_12925.java
@@ -0,0 +1,15 @@
+package Lv1;
+
+public class Solution_12925 {
+	public static void main(String[] args) {
+		String s1 = "1234";
+		String s2 = "-1234";
+
+		System.out.println(solution(s1));
+		System.out.println(solution(s2));
+	}
+
+	public static int solution(String s) {
+		return Integer.parseInt(s);
+	}
+}
