f28839606c82b3ba6f911c9a02011963ceffb244
H-JWANNA <hongjjwan@gmail.com>
2023-02-10 20:32:23 +0900

    직사각형 별찍기

---
+++ Programmers/Lv1/Solution_12969.java
@@ -0,0 +1,20 @@
+package Lv1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution_12969 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
+		for (int i = 0; i < m; i++) {
+			System.out.println("*".repeat(n));
+		}
+	}
+}
