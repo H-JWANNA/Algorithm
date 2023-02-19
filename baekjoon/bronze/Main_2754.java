8599eb13619fee50c6191d7f873ec694838c63bf
H-JWANNA <hongjjwan@gmail.com>
2023-02-19 23:59:29 +0900

    학점 계산

---
+++ baekjoon/bronze/Main_2754.java
@@ -0,0 +1,17 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_2754 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+
+		switch (s) {
+			case "A+":
+				System.out.println(4.0);
+		}
+	}
+}
