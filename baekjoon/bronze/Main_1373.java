dcd672d73ccb158b7f6a84e7c839867c2c380401
H-JWANNA <hongjjwan@gmail.com>
2023-03-05 23:34:03 +0900

    2진수 8진수

---
+++ baekjoon/bronze/Main_1373.java
@@ -0,0 +1,30 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_1373 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		StringBuilder sb = new StringBuilder();
+
+		if (s.length() % 3 == 1) {
+			sb.append(s.charAt(0) - '0');
+		}
+
+		if (s.length() % 3 == 2) {
+			sb.append((s.charAt(0) - '0') * 2
+				+ (s.charAt(1) - '0'));
+		}
+
+		for (int i = s.length() % 3; i < s.length(); i += 3) {
+			sb.append((s.charAt(i) - '0') * 4
+				+ (s.charAt(i + 1) - '0') * 2
+				+ (s.charAt(i + 2) - '0'));
+		}
+
+		System.out.println(sb);
+	}
+}
