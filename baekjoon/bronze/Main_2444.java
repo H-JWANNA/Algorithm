181638f1baaad37ccf552590690ee736892adfe4
H-JWANNA <hongjjwan@gmail.com>
2023-03-07 00:04:38 +0900

    별 찍기 - 7

--- baekjoon/bronze/Main_2444.java
+++ baekjoon/bronze/Main_2444.java
@@ -11,9 +11,12 @@ public class Main_2444 {
 
 		for (int i = 1; i <= n; i++) {
 			System.out.print(" ".repeat(n - i));
-			System.out.println("*".repeat(i));
-			System.out.println(" ".repeat(n - i));
-			System.out.println(" ".repeat(n - i));
+			System.out.println("*".repeat(i + (i - 1)));
+		}
+
+		for (int i = n - 1; i >= 1; i--) {
+			System.out.print(" ".repeat(n - i));
+			System.out.println("*".repeat(i + (i - 1)));
 		}
 	}
 }
