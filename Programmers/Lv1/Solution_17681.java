425cf751c928b04d14052a954641db2ec0ee4091
H-JWANNA <hongjjwan@gmail.com>
2023-03-08 22:38:09 +0900

    ACM 호텔

---
+++ Programmers/Lv1/Solution_17681.java
@@ -0,0 +1,19 @@
+package Lv1;
+
+public class Solution_17681 {
+	public static void main(String[] args) {
+		System.out.println(Integer.toBinaryString(3));
+	}
+	public static String[] solution(int n, int[] arr1, int[] arr2) {
+
+		String[] answer = new String[n];
+
+		for (int i = 0; i < n; i++) {
+			String binaryNum = Integer.toBinaryString(arr1[i]);
+
+
+		}
+
+		return new String[]{};
+	}
+}
