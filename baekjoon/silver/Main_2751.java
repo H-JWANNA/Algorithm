d6f96440029832ec0d776d0e9381fc1435216266
H-JWANNA <hongjjwan@gmail.com>
2023-03-28 23:47:33 +0900

    수 정렬하기 2

--- baekjoon/silver/Main_2751.java
+++ baekjoon/silver/Main_2751.java
@@ -19,7 +19,8 @@ public class Main_2751 {
 		}
 
 		// Arrays.sort()는 Quick 정렬을 사용하기 때문에 최악의 경우 O(n^2)의 시간 복잡도를 가짐
-		// Collections.sort()는 Tim 정렬(Insert + Merge 정렬)을 사용하기 때문에 O(NlogN)의 시간 복잡도를 가짐
+		// Collections.sort()는 Tim 정렬(Insertion + Merge 정렬)을 사용하기 때문에 O(NlogN)의 시간 복잡도를 가짐
+		// Stream의 sorted()는 Arrays.sort() 메서드에 정렬 작업을 위임한다.
 		Collections.sort(arr);
 
 		arr.forEach(i -> sb.append(i).append("\n"));
