ad6f9c77c784b410cab05ee2e429bd2d67cb240a
H-JWANNA <hongjjwan@gmail.com>
2023-05-08 20:32:22 +0900

    회의실 배정

--- baekjoon/silver/Main_1931.java
+++ baekjoon/silver/Main_1931.java
@@ -8,33 +8,43 @@ import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Main_1931 {
-  public static void main(String[] args) throws IOException {
-    BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 
-    int n = Integer.parseInt(input.readLine());
-    int[][] meeting = new int[n][];
-    int result = 0;
+		int n = Integer.parseInt(input.readLine());
+		int[][] meeting = new int[n][2];
+		int result = 0;
 
-    // 값 입력
-    for (int i = 0; i < n; i++) {
-      st = new StringTokenizer(input.readLine(), " ");
+		// 입력
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(input.readLine(), " ");
 
-      int[] time = new int[2];
-      time[0] = Integer.parseInt(st.nextToken());
-      time[1] = Integer.parseInt(st.nextToken());
+			meeting[i][0] = Integer.parseInt(st.nextToken());
+			meeting[i][1] = Integer.parseInt(st.nextToken());
+		}
 
-      meeting[i] = time;
-    }
+		// 종료 시간을 기준으로 정렬
+		Arrays.sort(meeting, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				return o1[1] != o2[1] ? o1[1] - o2[1] : o1[0] - o2[0];
+			}
+		});
 
-    // 2차원 배열 정렬
-    Arrays.sort(meeting, Comparator.comparingInt(o -> o[0]));
+		// System.out.println(Arrays.deepToString(meeting));
 
-    // meeting 배열 순회
-//    for (int i = 0; i < meeting.length; i++) {
-//      if (meeting[i][1] <= meeting[i + 1][0])
-//    }
+		int endTime = 0;
 
-    System.out.println(Arrays.deepToString(meeting));
-  }
+		for (int i = 0; i < meeting.length; i++) {
+			int startTime = meeting[i][0];
+
+			if (startTime >= endTime) {
+				endTime = meeting[i][1];
+				result += 1;
+			}
+		}
+
+		System.out.println(result);
+	}
 }
