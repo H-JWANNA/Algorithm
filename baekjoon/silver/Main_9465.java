fee0b356772746d373e8ffd71867c02031861070
H-JWANNA <hongjjwan@gmail.com>
2023-06-07 21:01:35 +0900

    스티커

--- baekjoon/silver/Main_9465.java
+++ baekjoon/silver/Main_9465.java
@@ -6,9 +6,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main_9465 {
-	static int[][] stickers;
 	static int[][] dp;
-	static int max;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -29,51 +27,46 @@ public class Main_9465 {
 
 		while (t-- > 0) {
 			int n = Integer.parseInt(br.readLine());
-			stickers = new int[2][n];
 			dp = new int[2][n];
-			max = 0;
 
 			for (int i = 0; i < 2; i++) {
 				st = new StringTokenizer(br.readLine(), " ");
 				for (int j = 0; j < n; j++) {
-					int point = Integer.parseInt(st.nextToken());
-					stickers[i][j] = point;
+					dp[i][j] = Integer.parseInt(st.nextToken());
 				}
+			}
+
+			// edge case
+			if (n == 1) {
+				System.out.println(Math.max(dp[0][0], dp[1][0]));
+				continue;
 			}
 
 			init();
 
 			// Bottom-up
-			for (int i = 2; i < n; i++) {
-				max = Math.max(max, sum(i));
+			for (int x = 2; x < n; x++) {
+				for (int y = 0; y < 2; y++) {
+					sum(y, x);
+				}
 			}
 
-			System.out.println(max);
+			System.out.println(Math.max(dp[0][n - 1], dp[1][n - 1]));
 		}
 
 	}
 
-	private static int sum(int n) {
-		dp[0][n] = Math.max(dp[1][n - 1] + stickers[0][n], dp[1][n - 2] + stickers[0][n]);
-		dp[1][n] = Math.max(dp[0][n - 1] + stickers[1][n], dp[0][n - 2] + stickers[1][n]);
-
-		return Math.max(dp[0][n], dp[1][n]);
+	private static void sum(int y, int x) {
+		dp[y][x] = Math.max(
+			dp[1 - y][x - 1] + dp[y][x],
+			dp[1 - y][x - 2] + dp[y][x]
+		);
 	}
 
 	private static void init() {
-		/*
-			== 초기값 설정 ==
-			0번째 열에는 원래 값
-			1번째 열에는 대각선 값의 합을 넣는다.
-		 */
-		for (int i = 0; i < 2; i++) {
-			dp[i][0] = stickers[i][0];
-			max = Math.max(max, dp[i][0]);
-
-			if (dp[0].length > 1) {
-				dp[i][1] = stickers[1 - i][0] + stickers[i][1];
-				max = Math.max(max, dp[i][1]);
-			}
+		// 1번째 열에는 대각선 합을 넣는다.
+		for (int y = 0; y < 2; y++) {
+			dp[y][1] = dp[1 - y][0] + dp[y][1];
 		}
 	}
 }
