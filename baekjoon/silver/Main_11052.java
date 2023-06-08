181bc067f3f59769ea06b24e606e100f0fe68c6d
H-JWANNA <hongjjwan@gmail.com>
2023-06-08 19:43:47 +0900

    카드 구매하기

---
+++ baekjoon/silver/Main_11052.java
@@ -0,0 +1,53 @@
+package silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_11052 {
+	static int[] cards;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int n = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		cards = new int[n + 1];
+
+		for (int i = 1; i < cards.length; i++) {
+			cards[i] = Integer.parseInt(st.nextToken());
+		}
+
+		/*
+			카드를 살 수 있는 경우의 수는 다음과 같다.
+			ex) 11개를 살 경우 => [11, 10 + 1, 9 + 2, 8 + 3, 7 + 4, 6 + 5]
+
+			따라서 (n - n / 2)번째까지 비교를 하며 최대가 되는 가격을 구한다.
+		 */
+
+		// dp 초기값
+		if (n > 1) {
+			cards[2] = Math.max(cards[1] * 2, cards[2]);
+		}
+
+		// Bottom-up
+		for (int i = 3; i < cards.length; i++) {
+			cards[i] = getMax(i);
+		}
+
+		System.out.println(cards[n]);
+	}
+
+	private static int getMax(int index) {
+		int mid = index - index / 2;
+		int max = 0;
+
+		for (int i = index; i >= mid; i--) {
+			max = Math.max(max, cards[i] + cards[index - i]);
+		}
+
+		return max;
+	}
+}
