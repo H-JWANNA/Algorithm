702c64759227123d09833eaac1269ca5498dcad5
H-JWANNA <hongjjwan@gmail.com>
2023-06-13 16:58:33 +0900

    IOIOI

--- baekjoon/silver/Main_5525.java
+++ baekjoon/silver/Main_5525.java
@@ -5,65 +5,40 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main_5525 {
-    static String s;
-    static int count = 0;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int m = Integer.parseInt(br.readLine());
+		String s = br.readLine();
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
-        s = br.readLine();
+		int count = 0;
+		int stack = 0;
+		int len = 2 * n + 1;
+		boolean I = false;
 
-        int len = 2 * n + 1;
-        int end = m - len;
+		for (int i = 0; i < m; i++) {
+			if (s.charAt(i) == 'I') {
+				if (!I) {
+					stack++;
+					I = true;
+				} else {
+					stack = 1;
+				}
+			} else {
+				if (I) {
+					stack++;
+					I = false;
+				} else {
+					stack = 0;
+				}
+			}
 
-        int i = 0;
+			if (stack == len) {
+				count++;
+				stack -= 2;
+			}
+		}
 
-        while (i <= end) {
-            int jump = search(i, len);
-            i += jump;
-        }
-
-        System.out.println(count);
-    }
-
-    private static int search(int start, int len) {
-        int sameLen = 0;
-        int idx = 0;
-
-        for (int i = start; i < s.length(); i++) {
-            if (idx % 2 == 0) {
-                if (s.charAt(i) == 'I') {
-                    sameLen++;
-                } else {
-                    break;
-                }
-            }
-
-            if (idx % 2 == 1) {
-                if (s.charAt(i) == 'O') {
-                    sameLen++;
-                } else {
-                    break;
-                }
-            }
-
-            idx++;
-
-            if (sameLen == len) {
-                count++;
-                break;
-            }
-        }
-
-        if (sameLen == 0) {
-            return 1;
-        } else if (sameLen == 1) {
-            return 1;
-        } else if (sameLen % 2 == 1) {
-            return 2;
-        } else {
-            return sameLen;
-        }
-    }
+		System.out.println(count);
+	}
 }
