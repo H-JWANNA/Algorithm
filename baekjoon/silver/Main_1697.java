f722e1f4ab82e6f808528740488cde7ccae3428b
H-JWANNA <hongjjwan@gmail.com>
2023-06-15 03:53:55 +0900

    숨바꼭질

---
+++ baekjoon/silver/Main_1697.java
@@ -0,0 +1,53 @@
+package silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main_1697 {
+	static boolean[] visited = new boolean[100001];
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+
+		System.out.println(bfs(n, k));
+	}
+
+	private static int bfs(int n, int k) {
+		int[] arr = new int[] {n, 0};
+
+		Queue<int[]> q = new ArrayDeque<>();
+		q.add(arr);
+
+		while (!q.isEmpty()) {
+			int[] cur = q.poll();
+			int node = cur[0];
+			int count = cur[1];
+
+			visited[node] = true;
+
+			if (cur[0] == k) {
+				return count;
+			}
+
+			if (node > 0 && !visited[node - 1]) {
+				q.add(new int[]{node - 1, count + 1});
+			}
+			if (node < 100000 && !visited[node + 1]) {
+				q.add(new int[]{node + 1, count + 1});
+			}
+			if (node != 0 && node <= 50000 && !visited[node * 2]) {
+				q.add(new int[]{node * 2, count + 1});
+			}
+		}
+
+		return -1;
+	}
+}
