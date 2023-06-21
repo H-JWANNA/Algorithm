1c9197e97f685f8ec304e0c65d3db364d44bb748
H-JWANNA <hongjjwan@gmail.com>
2023-06-21 22:20:47 +0900

    새싹

---
+++ baekjoon/bronze/Main_25083.java
@@ -0,0 +1,14 @@
+package bronze;
+
+public class Main_25083 {
+	public static void main(String[] args) {
+		String sb = "         ,r'\"7\n"
+			+ "r`-_   ,'  ,/\n"
+			+ " \\. \". L_r'\n"
+			+ "   `~\\/\n"
+			+ "      |\n"
+			+ "      |";
+
+		System.out.println(sb);
+	}
+}
