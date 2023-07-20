35d59f1a645f033aa2d80418ff293d2eee694a43
H-JWANNA <hongjjwan@gmail.com>
2023-07-20 21:39:20 +0900

    집합의 표현

--- baekjoon/gold/Main_1717.java
+++ baekjoon/gold/Main_1717.java
@@ -5,11 +5,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-/*
-	메모리 초과
- */
-
 public class Main_1717 {
+	static int[] parents;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
@@ -20,12 +18,16 @@ public class Main_1717 {
 		int m = Integer.parseInt(st.nextToken());
 
 		/*
-			연관 관계를 나타내는 2차원 배열을 생성한다.
-			0의 입력이 주어져서 합집합을 만들 경우, 연관 관계를 생성한다.
-			1의 입력이 주어져서 확인할 경우, 연관 관계가 생성되어 있다면 YES, 아니라면 NO를 출력한다.
+			초기 parents 배열은 자기 자신이 최상을 최상위 노드로 설정한다.
+			0을 입력받을 경우 a, b 노드의 값 중 최소 값을 각각 부모로 설정한다.
+			1을 입력받을 경우 a, b 노드의 부모가 같은지 확인하고 YES 혹은 NO를 출력한다.
 		 */
 
-		boolean[][] relation = new boolean[n + 1][n + 1];
+		parents = new int[n + 1];
+
+		for (int i = 0; i <= n; i++) {
+			parents[i] = i;
+		}
 
 		for (int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -33,29 +35,34 @@ public class Main_1717 {
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 
-			// 합집합 : 연관 관계 생성
-			if (op == 0) {
-				for (int j = 0; j <= n; j++) {
-					if (relation[a][j]) {
-						relation[b][j] = true;
-						relation[j][b] = true;
-					}
+			// 서로의 부모를 확인한다.
+			int x = search(a);
+			int y = search(b);
 
-					if (relation[b][j]) {
-						relation[a][j] = true;
-						relation[j][a] = true;
+			// 합집합 : 부모가 다르면 작은 값으로 부모를 통일시킨다.
+			if (op == 0) {
+				if (x != y) {
+					if (x < y) {
+						parents[y] = x;
+					} else {
+						parents[x] = y;
 					}
 				}
-
-				relation[a][b] = true;
-				relation[b][a] = true;
 			}
-			// 확인 : 연관 관계 확인
+			// 확인 : 부모가 같은지 확인한다.
 			else {
-				sb.append(relation[a][b] ? "YES" : "NO").append("\n");
+				sb.append(x == y ? "YES" : "NO").append("\n");
 			}
 		}
 
 		System.out.print(sb);
+	}
+
+	private static int search(int node) {
+		if (node == parents[node]) {
+			return node;
+		}
+
+		return parents[node] = search(parents[node]);
 	}
 }
